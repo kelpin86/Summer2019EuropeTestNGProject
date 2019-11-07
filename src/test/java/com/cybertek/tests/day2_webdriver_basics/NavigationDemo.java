58aa7725c5c621487edcc01867929405de6150f5
mustafa <kelpin86@gmail.ocm>
2019-11-07 21:31:35 +0000

    first commit
    Signed-off-by: mustafa <kelpin86@gmail.ocm>

---
+++ src/test/java/com/cybertek/tests/day2_webdriver_basics/NavigationDemo.java
@@ -0,0 +1,41 @@
+package com.cybertek.tests.day2_webdriver_basics;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NavigationDemo {
+    private static long millis;
+
+    public static void main(String[] args) {
+
+        //setting up the browser
+        WebDriverManager.chromedriver().setup();
+
+        //selenium object
+        WebDriver driver = new ChromeDriver();
+
+        //navigate to website
+        driver.get("https://wwww.google.com");
+
+        //another way of opening website
+        String url = "https://facebook.com";
+        //driver.navigate().to(url:"https://facebook.com");
+
+        //goes back to previous website
+        driver.navigate().back();
+
+
+        //Thread.sleep(millis:3000);
+        
+        //goes forward after goes back
+        
+        driver.navigate().forward();
+
+
+
+
+
+
+    }
+}
---
+++ src/test/java/com/cybertek/tests/day2_webdriver_basics/getUrlAndTitle.java
@@ -0,0 +1,8 @@
+package com.cybertek.tests.day2_webdriver_basics;
+
+public class getUrlAndTitle {
+    public static void main(String[] args) {
+
+
+    }
+}
